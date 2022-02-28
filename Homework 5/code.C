434d40dec947bed6eb39c3a6375869701bee749d
Casey Sobecks <csobe126@gmail.com>
2022-02-28 13:26:42 -0500

    plot range adjustment

--- Homework 5/code.C
+++ Homework 5/code.C
@@ -1,13 +1,13 @@
 //HW5 code
 void code(){
     auto c = new TCanvas("c","Error Function Graph",0,0,500,500);
-    double x, y, z, z1, r2=1.0, P = 6.0;
+    double x, y, z, z1, r2=2.0, P = 3.0;
     int np = 300;
     auto dt = new TGraph2D();
     auto r  = new TRandom();
     for (int N=0; N<np; N++) {
-        x = 2*P*(r->Rndm(N));
-        y = 2*P*(r->Rndm(N));
+        x = 2*P*(r->Rndm(N))-P;
+        y = 2*P*(r->Rndm(N))-P;
         z=sqrt(r2*r2+x*x+y*y);
         z1=erfc(z);
         dt->SetPoint(N,x,y,z1);
