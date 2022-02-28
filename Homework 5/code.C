4538cb65a2ceaf9e148add8c3d9e894c5b7a2669
Casey Sobecks <csobe126@gmail.com>
2022-02-28 12:47:48 -0500

    change of function

--- Homework 5/code.C
+++ Homework 5/code.C
@@ -1,6 +1,6 @@
 //HW5 code
 void code(){
-    auto c = new TCanvas("c","2D Graph",0,0,500,500);
+    auto c = new TCanvas("c","Error Function Graph",0,0,500,500);
     double x, y, z, z1, r2=1.0, P = 6.0;
     int np = 300;
     auto dt = new TGraph2D();
@@ -9,7 +9,7 @@ void code(){
         x = 2*P*(r->Rndm(N));
         y = 2*P*(r->Rndm(N));
         z=sqrt(r2*r2+x*x+y*y);
-        z1=erf(z)-1;
+        z1=erfc(z);
         dt->SetPoint(N,x,y,z1);
     }
     dt->Draw("tri1 p0");
